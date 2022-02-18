177333a0f1d306236c5ceb5cb467212a9b421c49
Alexander Rodriguez <adragonrc@gmail.com>
2022-02-18 17:02:17 -0500

    first commit

---
+++ PreventorSDKCore.xcframework/ios-arm64/PreventorSDKCore.framework/Headers/PreventorSDKCore.h
@@ -0,0 +1,18 @@
+//
+//  PreventorSDKCore.h
+//  PreventorSDKCore
+//
+//  Created by Alexander Rodriguez on 18/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PreventorSDKCore.
+FOUNDATION_EXPORT double PreventorSDKCoreVersionNumber;
+
+//! Project version string for PreventorSDKCore.
+FOUNDATION_EXPORT const unsigned char PreventorSDKCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreventorSDKCore/PublicHeader.h>
+
+
